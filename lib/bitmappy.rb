389a25eacebe7d289caf1db361a6ee3c564b4982
Chris Hughes <chris-git@fysh.org>
2015-06-28 11:52:04 +0100

    New gem for bitmappy

---
+++ lib/bitmappy.rb
@@ -0,0 +1,5 @@
+require "bitmappy/version"
+
+module Bitmappy
+  # Your code goes here...
+end
---
+++ test/bitmappy_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class BitmappyTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Bitmappy::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
